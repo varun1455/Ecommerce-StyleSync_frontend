4a035e182d857714c90a4d3eee0801b8d773eb8f
Varun Jain <83868029+varun1455@users.noreply.github.com>
2024-09-22 17:34:36 +0530

    changeinLogin

--- src/features/auth/components/Login.js
+++ src/features/auth/components/Login.js
@@ -17,6 +17,13 @@ export function Login() {
  
   const dispatch = useDispatch();
 
+  const [passwordVisible , setPasswordVisible] = useState(false);
+
+
+  const togglePasswordVisibility = () =>{
+    setPasswordVisible(!passwordVisible);
+  }
+
   const error = useSelector(selectError);
   console.log(error);
   const user = useSelector(selectLoggedInUser)
@@ -77,17 +84,41 @@ export function Login() {
                   </a>
                 </div>
               </div>
-              <div className="mt-2">
+              <div className="mt-2 position: relative">
                 <input
                   id="password"
                   {...register("password", { required: "password is required",
                     message: "passowrd is required or incorrect"
                    })}
-                  type="password"
+                  type={passwordVisible?"text": "password"}
                   // message="password not vaild"
                   
+                  
+                  
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+
                 />
+
+<span
+        onClick={togglePasswordVisibility}
+        style={{
+          position: "absolute",
+          right: "10px",
+          top: "50%",
+          transform: "translateY(-50%)",
+          cursor: "pointer",
+        }}
+      >
+       {passwordVisible ? <svg class="h-8 w-8 text-slate-400"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/>
+</svg> : <svg class="h-8 w-8 text-slate-400"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"/>
+</svg>
+
+}
+      </span>
+               
                 {error && <p className='text-red-500'>{error.message}</p>}
               </div>
             </div>
